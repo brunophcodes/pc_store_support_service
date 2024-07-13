9729b6b1eac65786663a674902210c3679335a99
Bruno Peña <brunoph26@gmail.com>
2024-07-12 21:41:11 -0500

    fix: store foreigh key to repair request, and change in the controller, add seeds

--- app/controllers/repair_requests_controller.rb
+++ app/controllers/repair_requests_controller.rb
@@ -1,5 +1,6 @@
 class RepairRequestsController < ApplicationController
-
+  before_action :find_repair_request, only: %i[show edit update destroy]
+ 
   def index 
     @repair_requests = RepairRequest.all
   end
@@ -9,10 +10,17 @@ class RepairRequestsController < ApplicationController
 
   def new
     @repair_request = RepairRequest.new
+    @categories = Category.all.map {|cat| cat.name } 
   end
 
   def create 
      @repair_request = RepairRequest.new(repair_request_params)
+     @repair_request.repair_status = 'Pending'
+
+     #Getting the available stores that matches the product category
+     @available_stores = Store.joins(:categories).where(categories: {name: @repair_request.product_category})
+     @repair_request.store_id = @available_stores.sort.first.id
+     @repair_request.save
   end
 
   def edit
@@ -20,10 +28,12 @@ class RepairRequestsController < ApplicationController
 
   def update
      @repair_request = RepairRequest.update(repair_request_params)
+     redirect_to repair_request_path(@repair_request)
   end
 
   def destroy
     @repair_request.destroy
+    redirect_to repair_request_path
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+p 'Deleting seeds'
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+User.destroy_all
+Category.destroy_all
+Store.destroy_all
+#RepairRequest.destroy_all
+
+p 'Creating seeds'
+
+#UserS:
+User.create!(email:'admin@gmail.com', password:'123456')
+
+
+#Stores:
+Store.create!(name: 'Tienda Miraflores', address: 'Begonias Street 220')
+Store.create!(name: 'Tienda Rimac', address: 'Felipe Arancibia Street 448')
+Store.create!(name: 'Tienda Cuzco', address: 'Tahuantinsuyo Avenue 1441')
+
+#Categories:
+Category.create!(name: 'Laptop', store_ids: [1,2])
+Category.create!(name: 'Smartphone', store_ids: [2])
+Category.create!(name: 'Videogames Console', store_ids: [1,3])
+Category.create!(name: 'TV', store_ids: [1,2,3])
+
+
