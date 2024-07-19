0f80b59b1e6befdfb845d90404daf7bae1f6f97f
Bruno Peña <brunoph26@gmail.com>
2024-07-19 00:29:02 -0500

    feat: add repair requests seeds & fix style on index table

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,12 @@ end
 User.destroy_all
 Category.destroy_all
 Store.destroy_all
-#RepairRequest.destroy_all
+RepairRequest.destroy_all
 
 p 'Creating seeds'
 
 #UserS:
-User.create!(email:'admin@gmail.com', password:'123456')
+User.create!(email:'admin@gmail.com', password:'admin123')
 
 
 #Stores:
@@ -37,3 +37,15 @@ Category.create!(name: 'Videogames Console', store_ids: [1,3])
 Category.create!(name: 'TV', store_ids: [1,2,3])
 
 
+#RepairRequests:
+
+RepairRequest.create!(client_email: 'anitakat@gmail.com', product_name: 'PlayStation 4', product_category: 'Videogames Console', repair_description: 'Disc reader is not working', repair_price: 50, repair_status: 'Aproved', store_id: 1 )
+RepairRequest.create!(client_email: 'martin2@gmail.com', product_name: 'Samsung 50" 4K OLED', product_category: 'TV', repair_description: 'The screen has some dead pixels', repair_price: 130, repair_status: 'Aproved', store_id: 1 )
+RepairRequest.create!(client_email: 'valeria_a@gmail.com', product_name: 'iPhone 12 Pro', product_category: 'Smartphone', repair_description: 'Battery is not charging', repair_price: 80, repair_status: 'Aproved', store_id: 2 )
+RepairRequest.create!(client_email: 'lucas44@gmail.com', product_name: 'Asus Tuf Gaming 15', product_category: 'Laptop', repair_description: "it's loud and heats up too fast", repair_price: 50 , repair_status: 'Aproved', store_id: 2 )
+RepairRequest.create!(client_email: 'joelt15@gmail.com', product_name: 'Smart ThinQ LG 55" 4K', product_category: 'TV', repair_description: 'There are weird lines near the border', repair_price: 120, repair_status: 'Aproved', store_id: 2 )
+RepairRequest.create!(client_email: 'emma29@gmail.com', product_name: 'Nitro Acer', product_category: 'Laptop', repair_description: 'the camera is not working', repair_price: 50 , repair_status: 'Aproved', store_id: 1 )
+RepairRequest.create!(client_email: 'sofia19@gmail.com', product_name: 'Samsung Galaxy S22', product_category: 'Smartphone', repair_description: 'Touchpad does not detect my motions or typing', repair_price: 80 , repair_status: 'Aproved', store_id: 2 )
+RepairRequest.create!(client_email: 'maria1980@gmail.com', product_name: 'Huawei Matebook D14', product_category: 'Laptop', repair_description: "the trackpad doesn't work", repair_price: 75 , repair_status: 'Aproved', store_id: 1 )
+RepairRequest.create!(client_email: 'hugo5@outlook.com', product_name: 'Xbox Series X', product_category: 'Videogames Console', repair_description: 'Cannot connect to internet', repair_price: 70, repair_status: 'Aproved', store_id: 3 )
+RepairRequest.create!(client_email: 'paula_lol@gmail.com', product_name: 'Samsung 65" Smart TV Crystal UHD', product_category: 'TV', repair_description: "it has no sound", repair_price: 0, repair_status: 'Denied', store_id: 1 )
